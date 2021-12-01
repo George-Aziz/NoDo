15383a87d85ed58c53e29accd8fa9aec9342f723
George Aziz <George.69076@live.com>
2021-12-01 11:50:54 +0800

    Initial Commit

---
+++ NoDo/NoDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoDoApp.swift
+//  NoDo
+//
+//  Created by George on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NoDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
